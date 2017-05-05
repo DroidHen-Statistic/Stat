40ffed93a327e0f3fc83a2f096838bbc63c49f7c
WJA <wja_1993@163.com>
2017-05-05 15:55:10 +0800

    Add figures: return_date_fit

--- data_fit.py
+++ data_fit.py
@@ -6,12 +6,12 @@ import pandas as pd
 from functools import reduce
 
 def func(x,a,b):
-	return a*b**x
+	return a*(b**x)
 
-def fitDateReturn():
+def fitDateReturn(date):
 	connection = MysqlConnection("218.108.40.13","wja","wja","wja")
 	sql = "select * from log_return_s_wja_1_percent where date = %s"
-	result = connection.query(sql,20170202)
+	result = connection.query(sql,date)
 	return_num = result[0][5:34]
 	for i in range(len(return_num)):
 		return_num[i] /= 100
@@ -25,29 +25,29 @@ def fitDateReturn():
 	y = [func(day,popt[0],popt[1]) for day in days]
 	plt.plot(days,y,'b--')
 	plt.show()
+	#plt.savefig("E:/python/stat/figures/return_date_fit/"+str(date)+".jpg")
 	connection.close()
 	return popt
 
-#test()
-popt = fitDateReturn()
-plt.cla()
-connection = MysqlConnection("218.108.40.13","wja","wja","wja")
+if __name__ == '__main__':
+	connection = MysqlConnection("218.108.40.13","wja","wja","wja")
+	sql = "select date from log_return_s_wja_1_percent"
+	result = connection.query(sql)
+	dates = sorted(list(set(reduce(lambda x,y : x + y, result))))
+	connection.close()
 
-sql = "select date from log_return_s_wja_1_percent"
-result = connection.query(sql)
-dates = sorted(list(set(reduce(lambda x,y : x + y, result))))
-
-for date in dates:
-	sql = "select * from log_return_s_wja_1_percent where date = %s" 
-	result = connection.query(sql,date)
-	return_num = result[0][5:34]
-	for i in range(len(return_num)):
-		return_num[i] /= 100
-	days = list(range(1,30))
-	plt.plot(days,return_num,'r--')
-	y = [func(day,popt[0],popt[1]) for day in days]
-	plt.plot(days,y,'b--')
-	plt.show()
-	plt.cla()
-
-connection.close()
+	for date in dates:
+		popt = fitDateReturn(date)
+		plt.cla()
+		# connection = MysqlConnection("218.108.40.13","wja","wja","wja")
+		# sql = "select * from log_return_s_wja_1_percent where date = %s" 
+		# result = connection.query(sql,date)
+		# return_num = result[0][5:34]
+		# for i in range(len(return_num)):
+		# 	return_num[i] /= 100
+		# days = list(range(1,30))
+		# plt.plot(days,return_num,'r--')
+		# y = [func(day,popt[0],popt[1]) for day in days]
+		# plt.plot(days,y,'b--')
+		# plt.show()
+		# plt.cla()
