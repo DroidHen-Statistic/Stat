a229fab6259dbdd7d6485251b5952e5b7d4174fa
WJA <wja_1993@163.com>
2017-05-10 10:22:38 +0800

    figures:return-date-fit: Use the first few days of a month to predict the user return rate of coming days

--- data_fit.py
+++ data_fit.py
@@ -8,24 +8,24 @@ from functools import reduce
 def func(x,a,b):
 	return a*(b**x)
 
-def fitDateReturn(date):
+def fitDateReturn(dates):
 	connection = MysqlConnection("218.108.40.13","wja","wja","wja")
 	sql = "select * from log_return_s_wja_1_percent where date = %s"
-	result = connection.query(sql,date)
-	return_num = result[0][5:34]
-	for i in range(len(return_num)):
-		return_num[i] /= 100
-	print(len(return_num))
-	days = list(range(1,30))
-	print(days)
-	print(len(days))
-	plt.plot(days,return_num,'r--')
-	popt, pcov = curve_fit(func, days, return_num)
-	print(popt)
-	y = [func(day,popt[0],popt[1]) for day in days]
-	plt.plot(days,y,'b--')
-	plt.show()
-	#plt.savefig("E:/python/stat/figures/return_date_fit/"+str(date)+".jpg")
+	for date in dates:
+		result = connection.query(sql,date)
+		return_num = result[0][5:34]
+		for i in range(len(return_num)):
+			return_num[i] /= 100
+		days = list(range(2,31))
+		plt.plot(days,return_num,'r--')
+		popt, pcov = curve_fit(func, days[0:7], return_num[0:7])
+		print(popt)
+		y = [func(day,popt[0],popt[1]) for day in days]
+		plt.plot(days,y,'b--')
+		plt.grid(True)
+		#plt.show()
+		plt.savefig("E:/python/stat/figures/return_date_fit/"+str(date)+".jpg")
+		plt.cla()
 	connection.close()
 	return popt
 
@@ -36,18 +36,4 @@ if __name__ == '__main__':
 	dates = sorted(list(set(reduce(lambda x,y : x + y, result))))
 	connection.close()
 
-	for date in dates:
-		popt = fitDateReturn(date)
-		plt.cla()
-		# connection = MysqlConnection("218.108.40.13","wja","wja","wja")
-		# sql = "select * from log_return_s_wja_1_percent where date = %s" 
-		# result = connection.query(sql,date)
-		# return_num = result[0][5:34]
-		# for i in range(len(return_num)):
-		# 	return_num[i] /= 100
-		# days = list(range(1,30))
-		# plt.plot(days,return_num,'r--')
-		# y = [func(day,popt[0],popt[1]) for day in days]
-		# plt.plot(days,y,'b--')
-		# plt.show()
-		# plt.cla()
+	popt = fitDateReturn(dates)
