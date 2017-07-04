0836291bfc1b236a8b5f6536a32eb2cb83cc1406
WJA <wja_1993@163.com>
2017-07-04 13:36:55 +0800

    modify the utils

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import config
 
 from datetime import datetime, timedelta
 from functools import reduce
+import types
 
 def get_path(base = config.base_dir, *paths):
 	ret = base
@@ -26,7 +27,11 @@ def log_table(log_type, game_id):
 	return "log_" + log_type + "_s_wja_" + gameid +"_1"
 
 def get_log_type_path(log_type, game_id):
-	return os.path.join(config.log_base_dir,log_type,"s_"+str(game_id),log_type +"_2")
+	return os.path.join(config.log_base_dir,"s_"+str(game_id),log_type +"_2")
+
+def get_log_path(log_type, game_id, date):
+	year, month, day = split_date(date)
+	return os.path.join(get_log_type_path(log_type, game_id), year, month, day)
 
 def get_log_tmp_path(log_type, game_id, date):
 	return get_path(config.log_tmp_dir, str(game_id), log_type, str(date))
@@ -34,7 +39,11 @@ def get_log_tmp_path(log_type, game_id, date):
 def get_log_type_tmp_path(log_type, game_id):
 	return get_path(config.log_tmp_dir, str(game_id), log_type)
 
-def union_dict(*objs):
+def item_used_total_file(game_id,date):
+	log_tmp_path = get_log_tmp_path("item_used",game_id,date)
+	return os.path.join(log_tmp_path, str(date) + "_total")
+
+def union_dict(*objs,f = lambda x,y: x + y, initial = 0):
 	"""
 	合并多个字典，相同的键，值相加
 	
@@ -48,9 +57,27 @@ def union_dict(*objs):
 	"""
 	keys = set(sum([list(obj.keys()) for obj in objs],[]))
 	total = {}  
-	for key in keys:  
-		total[key] = reduce(lambda x,y:x + y,[obj.get(key,0) for obj in objs])
-	return total  
+	for key in keys:
+		total[key] = reduce(f,[obj.get(key,initial) for obj in objs])
+	return total 
+
+# def union_dict(*objs,f = lambda x,y: x + y, initial = 0):
+# 	"""
+# 	合并多个字典，相同的键，值相加
+	
+# 	union_dict({'a':1, 'b':2, 'c':3}, {'a':2, 'b':3}) ----> {'a':3, 'b':5, 'c':3}
+	
+# 	Arguments:
+# 		*objs {dict} -- 要合并的字典
+	
+# 	Returns:
+# 		[dict] -- 合并后的字典
+# 	"""
+# 	keys = set(sum([list(obj.keys()) for obj in objs],[]))
+# 	total = {}  
+# 	for key in keys: 
+# 		total[key] = reduce(f,[obj.get(key,initial) for obj in objs])
+# 	return total  
 
 def date_to_int(date):
 	return int(datetime.strftime(date,"%Y%m%d"))
@@ -58,10 +85,13 @@ def date_to_int(date):
 def int_to_date(date):
 	return datetime.strptime(str(date),"%Y%m%d")
 
+def get_yesterday(date):
+	return date_to_int(int_to_date(date) - timedelta(days = 1))
+
 def gen_dates(b_date, days):
     day = timedelta(days=1)
     for i in range(days):
-        yield b_date + day*i
+        yield b_date + day * i
 
 def get_date_list(start, end):
     """
