1c88486887cf46d0542290212d440608c471c0e6
twh <twh@abc.com>
2017-07-04 19:30:14 +0800

    utils:get_log_dir_from_date

--- utils.py
+++ utils.py
@@ -18,16 +18,17 @@ def get_figure_path(*subfolder):
 	return get_path(base,*subfolder)
 
 def item_user_table(game_id):
-	return "user_item_s_" + str(game_id)
+	return "user_item_" + str(game_id)
 
 def item_item_table(game_id):
-	return "item_item_s_" + str(game_id)
+	return "item_item_" + str(game_id)
 
 def log_table(log_type, game_id, server_id = 1):
 	return "log_" + log_type + "_s_wja_" + str(game_id) +"_" + str(server_id)
 
+# 2是api的版本号，目前是2
 def get_log_type_path(log_type, game_id):
-	return os.path.join(config.log_base_dir,"s_"+str(game_id),log_type +"_2")
+	return os.path.join(config.log_base_dir,str(game_id),log_type +"_2")
 
 def get_log_path(log_type, game_id, date):
 	year, month, day = split_date(date)
@@ -103,6 +104,23 @@ def get_date_list(start, end):
     for d in gen_dates(start, (end-start).days + 1):
         dates.append(date_to_int(d))
     return dates
+
+# 返回文件夹列表
+def get_log_dir_from_date(start, end, log_type, game_id, server_id=1):
+    dates = get_date_list(start, end)
+    dirs=[]
+    for date in dates:
+        dirs.append( get_log_path(log_type, game_id, date))
+    return dirs
+
+# 读入log，生成list结构
+'''
+def get_data_from_log(log_file):
+    files = os.listdir(day_dir)
+    for(cr_file in files):
+        if():
+            continue;
+'''
 
 def split_date(date):
 	date_str = str(date)
