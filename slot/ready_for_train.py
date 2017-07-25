e59a9a44fafe54330a3a2e4f1f83c63eaae89dbb
WJA <wja_1993@163.com>
2017-07-25 14:13:12 +0800

    update

--- slot/ready_for_train.py
+++ slot/ready_for_train.py
@@ -84,6 +84,8 @@ def read_user_data(file_dir, seq_len, max_len):
             # 前后的金币差
             line = line.split(" ")
             cr_data[0] = float(line[1]) - float(line[-1])
+            if file_type == 0 and float(line[-1]) > 3300000:
+                break
             cr_data[1] = float(line[-1])
 
             line = f_bonus.readline().strip()
@@ -140,29 +142,63 @@ def gen_uid_vector(seq_len, max_len):
     return uid_2_vectors
 # exit()
 
-def train(x, y):
-    scaler = StandardScaler()
-    x = scaler.fit_transform(x)
+def train_plot(x, y, seq_len):
+    # scaler = StandardScaler()
+    # x = scaler.fit_transform(x)
     
     # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
     # print(y_test)
     x_train = x_test = x
     y_train = y_test = y
-    tmp = [x_[0] for x_ in x[-31:]]
-    print(np.mean(tmp))
-    odds_pay = np.mean(np.array(x[-31:])[:,-5:], axis = 0)
-    print(odds_pay)
-    odds_no_pay = np.mean(np.array(x[:-31])[:,-5:], axis = 0)
 
-    odds_pay_var = np.var(np.array(x[-31:])[:,-5:], axis = 0)
+    print([x_[1] for x_ in x[-31:]])
+    print(np.mean([x_[1] for x_ in x[-31:]]))
+    plt.plot([x_[1] for x_ in x[-31:]],'b-o')
+    plt.show()
+    plt.cla()
+
+
+    path = file_util.get_figure_path("slot","1675766")
+    plt.figure(1)
+    for i in range(seq_len):
+        plt.plot([x_[-seq_len + i] for x_ in x[-31:]],'b-o')
+        plt.savefig(os.path.join(path, "odds"+str(i)))
+        plt.cla()
+
+    for i in range(5):
+        for j in range(5):
+            plt.plot(x[-31 + i*5 +j][-10:], '-o')
+        plt.savefig(os.path.join(path, "odds_seq_" + str(i)))
+        plt.cla()
+    # for seq in x[-31:]:
+    #     plt.plot(seq[-seq_len:], '-o')
+    # plt.savefig(os.path.join(path, "odds_seq"))
+    # plt.show()
+
+    # tmp = [x_[0] for x_ in x[-31:]]
+    # print(np.mean(tmp))
+    
+    odds_pay = np.mean(np.array(x[-31:])[:,-seq_len:], axis = 0)
+    print(odds_pay)
+    odds_no_pay = np.mean(np.array(x[:-31])[:,-seq_len:], axis = 0)
+    print(odds_no_pay)
+
+    odds_pay_var = np.var(np.array(x[-31:])[:,-seq_len:], axis = 0)
     print(odds_pay_var)
-    odds_no_pay_var = np.var(np.array(x[:-31])[:,-5:], axis = 0)
+    odds_no_pay_var = np.var(np.array(x[:-31])[:,-seq_len:], axis = 0)
     print(odds_no_pay_var)
 
-    plt.plot(odds_pay_var, label = 'pay')
-    plt.plot(odds_no_pay_var,label = 'no_pay')
+    plt.figure(2)
+    plt.plot(odds_pay, '-o',label = 'pay')
+    plt.plot(odds_no_pay,'-o',label = 'no_pay')
     plt.legend(loc = "upper right")
-    plt.show()
+    plt.savefig(os.path.join(path, "odds_mean"))
+
+    plt.figure(3)
+    plt.plot(odds_pay_var, '-o', label = 'pay')
+    plt.plot(odds_no_pay_var,'-o', label = 'no_pay')
+    plt.legend(loc = "upper right")
+    plt.savefig(os.path.join(path, "odds_var"))
 
     # clfs = [tree.DecisionTreeClassifier(max_depth = 5),
         # RandomForestClassifier(max_depth = 5),ExtraTreesClassifier(),AdaBoostClassifier(),GradientBoostingClassifier(),GaussianNB()]
@@ -171,8 +207,8 @@ def train(x, y):
     for clf in clfs:
         pipe_lr = Pipeline([('clf', clf)])
 
-        # recall = np.mean(cross_val_score(clf, x_train,
-        #                         y_train, scoring="recall", cv=5))
+        recall = np.mean(cross_val_score(clf, x_train,
+                                y_train, scoring="recall", cv=5))
         # precision = np.mean(cross_val_score(clf, x_train,
         #                         y_train, scoring="precision", cv=5))
         # roc_auc = np.mean(cross_val_score(clf, x_train,
@@ -183,14 +219,14 @@ def train(x, y):
         pipe_lr.fit(x_train, y_train)
         y_predict = pipe_lr.predict(x_test)
         pay_count = np.sum(y_test)
-        print(pipe_lr.named_steps['clf'].theta_)
-        print(pipe_lr.named_steps['clf'].sigma_)
+        # print(pipe_lr.named_steps['clf'].theta_)
+        # print(pipe_lr.named_steps['clf'].sigma_)
         # print(pay_count)
         recall = 0
         for i in range(len(y_test)):
             if y_test[i] == 1 and y_predict[i] == 1:
                 recall += 1
-        # print('Test accuracy: %.3f pay_count: %d recall: %d' % (pipe_lr.score(x_test, y_test), pay_count, recall))
+        print('Test accuracy: %.3f pay_count: %d recall: %d' % (pipe_lr.score(x_test, y_test), pay_count, recall))
 
    
     # clf = RandomForestClassifier(max_depth = 5)
@@ -215,10 +251,47 @@ def train(x, y):
     # graph = pydotplus.graph_from_dot_data(dot_data) 
     # graph.write_pdf(os.path.join(path,"test.pdf"))
 
+def train(x, y):
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
+    
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
+    # print(y_test)
+    x_train = x_test = x
+    y_train = y_test = y
+    # tmp = [x_[0] for x_ in x[-31:]]
+    # print(np.mean(tmp))
+    
+
+    clfs = [tree.DecisionTreeClassifier(max_depth = 5),
+        RandomForestClassifier(max_depth = 5),ExtraTreesClassifier(),AdaBoostClassifier(),GradientBoostingClassifier(),GaussianNB()]
+
+    for clf in clfs:
+        pipe_lr = Pipeline([('clf', clf)])
+
+        recall = np.mean(cross_val_score(clf, x_train,
+                                y_train, scoring="recall", cv=5))
+        # precision = np.mean(cross_val_score(clf, x_train,
+        #                         y_train, scoring="precision", cv=5))
+        # roc_auc = np.mean(cross_val_score(clf, x_train,
+        #                         y_train, scoring="roc_auc", cv=5))
+        # f1 = np.mean(cross_val_score(clf, x_train,
+        #                         y_train, scoring="f1", cv=5))
+        # print(recall, precision, roc_auc)
+        pipe_lr.fit(x_train, y_train)
+        y_predict = pipe_lr.predict(x_test)
+        pay_count = np.sum(y_test)
+        # print(pay_count)
+        recall = 0
+        for i in range(len(y_test)):
+            if y_test[i] == 1 and y_predict[i] == 1:
+                recall += 1
+        print('Test accuracy: %.3f pay_count: %d recall: %d' % (pipe_lr.score(x_test, y_test), pay_count, recall))
+
 if __name__ == '__main__':
     file_names = ['coin', 'is_free', 'level', 'odds',
               'time_delta', 'win_bonus', 'win_free']
-    seq_len = 5
+    seq_len = 10
     max_len = 10
     uid_2_vectors = gen_uid_vector(seq_len, max_len)
     # for uid, vectors in uid_2_vectors.items():
@@ -227,7 +300,7 @@ if __name__ == '__main__':
     # # Y = np.array(sum([x[1] for x in uid_2_vectors.values()],[]))
     #     train(vectors[0], vectors[1])
 
-    train(uid_2_vectors["1675766"][0], uid_2_vectors["1675766"][1])
+    train_plot(uid_2_vectors['1675766'][0], uid_2_vectors['1675766'][1], seq_len)
 
 
 
