4450310756f9dc8de6ea5e950b323dc6527d5e79
fcmorrowind <mew2_149@163.com>
2017-08-14 16:24:11 +0800

    add flip dict

--- utils/other_util.py
+++ utils/other_util.py
@@ -7,6 +7,11 @@ import numpy as np
 from functools import reduce
 import numpy as np
 
+"""
+反转字典的k和v
+"""
+def flip_dict(d):
+    return { pair[1]:pair[0] for pair in d.items()}
 
 def union_dict(*objs, f=lambda x, y: x + y, initial=0):
     """
