779d6ba425c58ca7b67ef131de0838e1ecb98369
fcmorrowind <mew2_149@163.com>
2017-10-11 17:43:30 +0800

    update

--- sample_codes/CRTree.py
+++ sample_codes/CRTree.py
@@ -153,60 +153,122 @@ class UserData:
         # self.y = self.vectors[:,0].astype(int)
 
 if __name__ == "__main__":
-    user_data = UserData(5)
+    user_data = UserData(pay_weight=5)
     # user_data.get_uid_2_vector(["bonus_times", "active_days", "locale"])
     user_data.get_uid_2_vector()
 
     from sklearn.model_selection import GridSearchCV
     from sklearn.model_selection import RandomizedSearchCV
+    from sklearn.model_selection import StratifiedKFold
 
 
     from sklearn import tree # 树形结构
+    from sklearn.metrics import recall_score
+    from sklearn.metrics import average_precision_score
+    from sklearn.metrics import accuracy_score
 
-    for test_time in range(0, 10):
+
+    # 要选的超参数
+    min_samples_per_node = list(range(16,64))
+    max_deep = list(range(4,8))
+
+
+    # path = file_util.get_figure_path("slot", "predict_purchase", "DT")
+    path = file_util.get_figure_path("slot", "predict_purchase", "TestParams")
+    acc_threshold = 0.75
+    import matplotlib.pyplot as plt
+    fig = plt.gcf()
+    fig.set_size_inches(15,12)
+    axMean = fig.add_subplot(211)  # 增加一个子图，注意，fig要用add
+    axStd = fig.add_subplot(212)  # 增加一个子图，注意，fig要用add
+    # figMean = plt.figure(1)
+    # figStd = plt.figure(2)
+    # figMean.set_size_inches(12,6)
+    # figStd.set_size_inches(12,6)
+    # axMean =figMean.gca()
+    # axStd =figStd.gca()
+
+    ##################### 换x坐标，这几个一定要搭配起来用！#############################
+    xlim = [0, len(min_samples_per_node)] # 把显示范围固定
+    # ylim = [0, 1]
+    axMean.set_xlim(xlim)
+    # axMean.set_ylim(ylim)
+    axMean.yaxis.set_major_locator(plt.LogLocator(2.1, [2.0])) # 按对数画刻度,第一个参数底数，第二个参数是倍数。比如这个例子在1,3,9,27...上标注刻度，也在2,6,12...上标注
+    # axMean.set_yscale("log", basey=2)  # 对数坐标轴
+    axMean.xaxis.set_major_locator(plt.MultipleLocator(1)) # 按1步长标示刻度
+    axMean.set_xticklabels([-1] + min_samples_per_node) # 补一个-1，因为坐标原点强制不显示，不补就错位
+    # # ax.set_xticklabels([-1] + max_deep) # 补一个-1，因为坐标原点强制不显示，不补就错位
+
+    axStd.set_xlim(xlim)
+    axStd.xaxis.set_major_locator(plt.MultipleLocator(1)) # 按1步长标示刻度
+    axStd.set_xticklabels([-1] + min_samples_per_node) # 补一个-1，因为坐标原点强制不显示，不补就错位
+    # # ax.set_xticklabels([-1] + max_deep) # 补一个-1，因为坐标原点强制不显示，不补就错位
+    ##################### 换x坐标，这几个一定要搭配起来用！#############################
+    axMean.set_title('means')
+    axStd.set_title('stds')
+    axMean.set_xlabel('min_sample')
+    axStd.set_xlabel('min_sample')
+    axMean.set_ylabel('recall_mean')
+    axStd.set_ylabel('recall_std')
+    # skf = StratifiedKFold(n_splits =3, shuffle =True)
+    for test_time in range(0, 1):
         print("test time:%s" % test_time)
         user_data.get_X_y()
+        X= user_data.X
+        y = user_data.y
         # min_samples_per_node = [16,32,64,128]
-        min_samples_per_node = list(range(16,64))
         scores = [] # 得分
-        variance = [] # 均值
+        stds = [] # 均值
         scoring = "recall"
         for min_sample in min_samples_per_node:
-            cls = tree.DecisionTreeClassifier(max_depth=7, min_samples_split= min_sample, min_samples_leaf= min_sample)
-            est = other_util.ScoreEstimator(cls, "recall", cv=3)
-            score_ret = est.score(user_data.X, user_data.y)
-            scores.append(np.mean(score_ret))
-            cr_var = np.array(score_ret).var()
-            variance.append(cr_var)
+        # for deep in max_deep:
+            cls = tree.DecisionTreeClassifier(max_depth=7, min_samples_split= min_sample, min_samples_leaf= min_sample, class_weight= {0: 1, 1: 5})
+            cvClassfier = other_util.KFoldsClassifier(cls, ["recall", "accuracy"], n_splits =3, shuffle =True)
+            cr_scores = cvClassfier.score(X, y)
+            if cr_scores["mean"]["accuracy"] < acc_threshold:
+                scores.append(0)
+                stds.append(0)
+            else:
+                scores.append(cr_scores["mean"]["recall"] ** 2 *100)    
+                stds.append(cr_scores["std"]["recall"])
 
+            # dot_data = tree.export_graphviz(cvClassfier.cls_ , out_file=None, 
+            #                                                             feature_names=UserData.features, 
+            #                                                             class_names=["no_pay", "pay"],
+            #                                                             filled=True, rounded=True,
+            #                                                             impurity=False)
+            # graph = pydotplus.graph_from_dot_data(dot_data)
+            # graph.write_pdf(os.path.join(path, "tree%s.pdf" % test_time))
+
+
+            # scores.append(np.mean(score_ret))
+            # cr_var = np.array(score_ret).var()
+            # variance.append(cr_var)
         # print(scores)
         # print(variance)
 
-        import matplotlib.pyplot as plt
-        ax =plt.gca()
-        plt.gcf().set_size_inches(12,6)
-        ax.plot(scores, label = "score")
-        ax.plot(variance, label = "var")
-        ax.set_title('node size')
-        ax.set_xlabel('min_sample')
-        ax.set_ylabel('recall')
-        ##################### 换x坐标，这几个一定要搭配起来用！#############################
-        xlim = [0, None] # 把显示范围固定
-        ax.set_xlim(xlim)
-        ax.xaxis.set_major_locator(plt.MultipleLocator(1)) # 按1步长标示刻度
-        ax.set_xticklabels([-1] + min_samples_per_node) # 补一个-1，因为坐标原点强制不显示，不补就错位
-        ##################### 换x坐标，这几个一定要搭配起来用！#############################
-        ax.legend(loc="upper right")
-
-        figure_path = file_util.get_figure_path("slot", "predict_purchase", "TestParams")
-        file_name = os.path.join(figure_path, "tree_" + str(test_time) + ".png")
-        # gcf.savefig(file_name, dpi= 160)
-        # if payed >= 1:
-        # gcf.savefig(file_name)
-        plt.savefig(file_name)
-        plt.cla()
-
+        # import matplotlib.pyplot as plt
+        # plt.gcf().set_size_inches(12,6)
+        scores = np.logspace(0,9,10, base =2)
+        axMean.plot(scores)
+        axStd.plot(stds)
         # plt.show()
+
+        # # ax.plot(variance, label = "var")
+        # ax.legend(loc="upper right")
+    figure_path = file_util.get_figure_path("slot", "predict_purchase", "TestParams")
+    file_name = os.path.join(figure_path, "tree_"  + "node" + ".png")
+    # file_name_std = os.path.join(figure_path, "tree_"  + "std" + ".png")
+    # gcf.savefig(file_name, dpi= 160)
+    # if payed >= 1:
+    # gcf.savefig(file_name)
+    plt.show()
+    fig.savefig(file_name)
+    # figMean.savefig(file_name)
+    # figStd.savefig(file_name_std)
+    plt.cla()
+    # figStd.cla()
+    # plt.show()
 
     exit()
 
@@ -251,9 +313,33 @@ if __name__ == "__main__":
     myTree = tree.DecisionTreeClassifier()
 
 
-
-
     # k = UserData.locales_2_num("US")
     # kk = UserData.locales_2_num("BB")
 
 
+
+
+
+ # 画ROC曲线
+probas_ = pipe_lr.predict_proba(x_test) 
+# print(pipe_lr.classes_ )
+
+# 通过roc_curve()函数，求出fpr和tpr，以及阈值  
+# 这里传进去的第二个参数是预测为正例的概率
+# The predicted class probability is the fraction of samples of the same class in a leaf.
+fpr, tpr, thresholds = roc_curve(y_test, probas_[:, 1])
+roc_auc = auc(fpr, tpr)
+#画图，只需要plt.plot(fpr,tpr),变量roc_auc只是记录auc的值，通过auc()函数能计算出来  
+plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (1, roc_auc))
+
+#画对角线
+plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
+
+
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
